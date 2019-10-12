2bc3b02b34df2b7dd639ba13031f34c441e67cd7
Janet <jaurinu@gmail.com>
2019-10-12 14:22:57 -0500

    add UI proyect

---
+++ PersonalityQuiz/PersonalityQuiz-ECC/PersonalityQuiz-ECC/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz-ECC
+//
+//  Created by Apple Device 9 on 10/12/19.
+//  Copyright © 2019 Squad-Dios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
